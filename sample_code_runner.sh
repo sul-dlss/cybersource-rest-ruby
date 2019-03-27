33cfc577c18d4a13bbcab368319e37c22b79cc12
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2019-03-27 18:56:17 +0530

    Updating output to log

--- sample_code_runner.sh
+++ sample_code_runner.sh
@@ -8,12 +8,13 @@ set -e
 while IFS="" read -r p || [ -n "$p" ]
 do
   if [[ "$p" =~ $(echo ^\($(sed 's/[[:blank:]]//g' sampleCodeIgnoreList.txt | paste -sd '|' /dev/stdin)\)$) ]]; then
-   printf '\n\n**** SKIPPED - %s ####\n' "$p"
-   printf '\n\n**** SKIPPED - %s ####\n' "$p" >> output.log
+   printf '\n\n#### SKIPPED - %s ####\n' "$p"
+   printf '\n\n#### SKIPPED - %s ####\n' "$p" >> output.log
   else
    printf '\n\n**** RUNNING - %s ****\n' "$p"
-   printf '\n\n**** RUNNING - %s ****\n' "$p" >> output.log
+   printf '\n\n**** START RUNNING - %s ****\n' "$p" >> output.log
    ruby $p >> output.log
+   printf '\n\n**** END RUNNING - %s ****\n' "$p" >> output.log
    sleep 3s
    FILECOUNT=$((FILECOUNT+1))
   fi
