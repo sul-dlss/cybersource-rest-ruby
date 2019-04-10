75e584f405e25cc3f477e21b39e97dcd566a935d
KarthikeyanKumar <karthikeyan.cse.skcet@gmail.com>
2019-04-10 16:23:17 +0530

    correcting name

--- Samples/Reporting/CoreServices/GetConversionDetailTransactions.rb
+++ Samples/Reporting/CoreServices/GetConversionDetailTransactions.rb
@@ -1,25 +1,24 @@
-require 'cybersource_rest_client'
+  require 'cybersource_rest_client'
 require_relative '../../../data/Configuration.rb'
 
 public
-class GetNetFundingInformation
+class GetConversionDetailTransactions
   def main()
     config = MerchantConfiguration.new.merchantConfigProp()
     start_time = "2019-03-21T00:00:00.0Z"
     end_time="2019-03-21T23:00:00.0Z"
     api_client = CyberSource::ApiClient.new
-    api_instance = CyberSource::NetFundingsApi.new(api_client, config)
+    api_instance = CyberSource::ConversionDetailsApi.new(api_client, config)
 
     opts = {}
     opts[:'organization_id'] = "testrest"
-    opts[:'group_name'] = "groupName"
 
-    data, status_code, headers = api_instance.get_net_funding_info(start_time,end_time)
+    data, status_code, headers = api_instance.get_conversion_detail(start_time,end_time)
     puts data, status_code, headers
   rescue StandardError => err
     puts err.message
   end
-  GetNetFundingInformation.new.main
+  GetConversionDetailTransactions.new.main
 end
 
 
