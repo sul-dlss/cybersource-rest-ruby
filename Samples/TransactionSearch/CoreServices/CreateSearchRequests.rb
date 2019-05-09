241381ee904da67037ae6d1ebd3466801467d5ad
Kaushik <kikmak42@null.net>
2019-05-09 18:23:28 +0530

    Fixes to Sample Code

---
+++ Samples/TransactionSearch/CoreServices/CreateSearchRequests.rb
@@ -0,0 +1,25 @@
+require 'cybersource_rest_client'
+require_relative '../../../data/Configuration.rb'
+
+public
+class CreateSearchRequests
+  def main()
+    config = MerchantConfiguration.new.merchantConfigProp()
+    request = CyberSource::CreateSearchRequest.new
+    api_client = CyberSource::ApiClient.new
+    api_instance = CyberSource::SearchTransactionsApi.new(api_client, config)
+    request.save = "false"
+    request.name = "MRN"
+    request.timezone = "America/Chicago"
+    request.query = "clientReferenceInformation.code:TC50171_3"
+    request.offset = 0
+    request.limit = 100
+    request.sort = "id:asc,submitTimeUtc:asc"
+    data, status_code, headers = api_instance.create_search(request)
+    puts data, status_code, headers
+  rescue StandardError => err
+    puts err.message
+  end
+  CreateSearchRequests.new.main
+end
+
