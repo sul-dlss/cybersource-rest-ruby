67a7aa2ac0f658fa3d7d4d0dab6c1a940c7d08cf
snavinch <snavinch@visa.com>
2019-06-17 15:19:10 +0530

    + Changes to transaction batches sample code

--- Samples/TransactionBatches/CoreServices/GetIndividualBatchFile.rb
+++ Samples/TransactionBatches/CoreServices/GetIndividualBatchFile.rb
@@ -8,7 +8,7 @@ class GetIndividualBatchFile
     config = MerchantConfiguration.new.merchantConfigProp()
     api_client = CyberSource::ApiClient.new
     api_instance = CyberSource::TransactionBatchesApi.new(api_client, config)
-    data, status_code, headers = api_instance.pts_v1_transaction_batches_id_get(id)
+    data, status_code, headers = api_instance.get_transaction_batch_id(id)
     puts data, status_code, headers
   rescue StandardError => err
     puts err.message
